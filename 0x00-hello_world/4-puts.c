04bc30ea3e072351b85aff9b14a61dd5b6cca148
Machiek <bithwilly@gmail.com>
2022-09-08 16:48:14 -0700

    start to code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 
 /**
- * main -Entry point.
  *
- * Use the function puts.
- * You are not allowed to use printf.
- * Your program should end with the value zero
+ * main -Entry point
+ *
+ * Use the function puts
+ *
+ * You are not allowed to use printf
+ *
+ * Your program should end with the value 0
  *
  * Return: 0
+ *
  */
+
 int main(void)
+
 {
-    puts("\"programming is like building a multilingual puzzle");
-    return (0);
+
+        puts("/"programming is like building a multilingual puzzle");
+
+        return (0);
 }
+
+
+        
+        
+
+ 
