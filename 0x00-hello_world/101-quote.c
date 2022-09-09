1edbed642b9d47add32b24497e9ebce21b0cfbf3
Machiek <bithwilly@gmail.com>
2022-09-08 17:59:31 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * You are not allowed to use any functions listed in the NAME section,
+ * of the (3) printf or man (3) puts.
+ * Your program should compile without any warnings when using the Wall,
+ * gcc option.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
+}
+
