5e55294f298b4740f6526b14a8174bf9d594ca2c
Machiek <bithwilly@gmail.com>
2022-09-09 08:04:22 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,18 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - Entry point.
+ * main - Entry point
  *
- * You are not allowed to use any function listed int the NAME section,
- * of the man (3) printf or man (3) puts.
- * Your program should return 1.
- * Your program should compile without any warnings when using the -wall,
- * gcc option.
- *
- * Return 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
