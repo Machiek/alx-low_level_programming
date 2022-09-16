15dca92c94feba5413001684433d13a9aa6ed836
Machiek <bithwilly@gmail.com>
2022-09-16 15:38:19 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "man.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; 1++)
+	{
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
+
