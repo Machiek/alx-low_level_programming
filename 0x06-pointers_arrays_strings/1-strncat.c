0572e59d2f7b0143873b91673dd65c031048037f
Machiek <bithwilly@gmail.com>
2022-10-03 12:32:08 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncat - function that concatenates two strings
+ * @dest: Pointer to the null-terminated byte string to append to.
+ * @src: pointer to the character array to copy from.
+ * @n: maximum number of characters to copy.
+ *
+ * Return: pointer to dest.
+ */
+
+
+char *_strncat(char *char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
